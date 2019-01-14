ad431545e4d5a6e452136b0e0693dbfcd67ff4be
anectto <anectto@@gmail.com>
2019-01-14 14:29:53 +0900

    Added views to create/edit blog post inside the site.

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from django.conf.urls import url
-from .import views
-
+from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
-   # path('', views.post_detail, name='post_detail'),
-    url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+    path('post/new', views.post_new, name='post_new'),
+
 ]
